c4da3ebdfffee140a67130d323c5d19922f31e50
Nichita Riscovoi <nichitariscovoi@gmail.com>
2024-10-09 16:57:58 +0100

    added a js file configuring firebase database

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCWQkEmFkxDqsWi_ilIbEdOLpPcGu9kWrg",
+    authDomain: "mtu-group-project-act.firebaseapp.com",
+    databaseURL: "https://mtu-group-project-act-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mtu-group-project-act",
+    storageBucket: "mtu-group-project-act.appspot.com",
+    messagingSenderId: "677743889534",
+    appId: "1:677743889534:web:0829f8e93b5960b407dc2f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
